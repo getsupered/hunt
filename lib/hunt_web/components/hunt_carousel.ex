3d238bb394c5395275e49c78eb45736b14e936e0
Stephen Bussey <sb8244@cs.ship.edu>
2023-09-04 16:16:19 -0400

    leaderboard buttons

--- lib/hunt_web/components/hunt_carousel.ex
+++ lib/hunt_web/components/hunt_carousel.ex
@@ -21,8 +21,8 @@ defmodule HuntWeb.HuntCarousel do
 
               <ul class="mx-4 my-4 space-y-4">
                 <%= if @user do %>
-                  <li class="rounded px-4 py-3 dropped-border-sm flex items-start gap-4">
-                    <.link patch={~p"/?leaderboard=open"} class="btn btn-primary flex items-center w-full text-center gap-2 text-xl">
+                  <li>
+                    <.link patch={~p"/?leaderboard=open"} class="rounded px-4 py-3 dropped-border-sm btn btn-primary flex items-center w-full text-center gap-2 text-xl">
                       <.icon name="hero-trophy" class="h-6 w-6" /> View Leaderboard
                     </.link>
                   </li>
@@ -66,6 +66,14 @@ defmodule HuntWeb.HuntCarousel do
               </div>
 
               <ul class="mx-4 my-4 space-y-4">
+                <%= if @user do %>
+                  <li>
+                    <.link patch={~p"/?leaderboard=open"} class="rounded px-4 py-3 dropped-border-sm btn btn-muted flex items-center w-full text-center gap-2 text-xl">
+                      <.icon name="hero-trophy" class="h-6 w-6" /> View Leaderboard
+                    </.link>
+                  </li>
+                <% end %>
+
                 <li class="rounded px-4 py-3 dropped-border-sm flex items-start gap-4">
                   <div class="flex-grow flex items-center">
                     <%= if @completion[slide].achievement do %>
